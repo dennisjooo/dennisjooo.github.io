6a87b466447f7a70f67cebe517c79de27232058c
Dennis Jonathan <dennis.jonathan78@gmail.com>
2023-07-11 17:17:41 +0700

    Added Skills & Contacts A lot of changes

--- js/main.js
+++ js/main.js
@@ -55,4 +55,20 @@
       window.addEventListener('load', headerScrolled)
       onscroll(document, headerScrolled)
     }
+
+    const items = document.querySelectorAll('.appear');
+
+    const active = function(entries){
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+            entry.target.classList.add('inview'); 
+            }else{
+                entry.target.classList.remove('inview'); 
+            }
+        });
+    }
+    const io = new IntersectionObserver(active);
+    for(let i=0; i < items.length; i++){
+        io.observe(items[i]);
+ }
 })()
