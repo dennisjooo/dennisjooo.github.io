f1ffeee07631cf0b079ccce79e03e4f124f0c7fa
Dennis Jonathan <dennis.jonathan78@gmail.com>
2023-09-01 13:49:59 +0700

    Revert "Fixes to `index.html` `projects.css` and `main.js`"
    This reverts commit 8f752c27a54d7607f6b65b4c81134a2e3f34b41a.

--- js/main.js
+++ js/main.js
@@ -30,17 +30,6 @@
     }
 
     /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos,
-      behavior: 'smooth'
-    })
-  }
-
-    /**
      * Easy on scroll event listener 
      */
     const onscroll = (el, listener) => {
