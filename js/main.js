a770331beeee3bdd5922dad6f323814e7fa6b5e7
Dennis Jonathan <dennis.jonathan78@gmail.com>
2023-09-01 14:04:07 +0700

    Lmao

--- js/main.js
+++ js/main.js
@@ -30,6 +30,17 @@
     }
 
     /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
+
+    /**
      * Easy on scroll event listener 
      */
     const onscroll = (el, listener) => {
