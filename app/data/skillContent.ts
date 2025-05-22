6f0db6ce1bf83e14042d7026282006f057da0899
Dennis Jonathan <dennisjonathan78@gmail.com>
2025-05-22 14:07:56 +0700

    Remove 'css' from skillIcons array in skillContent.ts for updated technology representation

--- app/data/skillContent.ts
+++ app/data/skillContent.ts
@@ -1,5 +1,5 @@
 export const skillIcons = [
-    'python', 'postgresql', 'javascript', 'html5', 'css', 'typescript', 
+    'python', 'postgresql', 'mongodb', 'javascript', 'html5', 'typescript', 
     'tensorflow', 'pytorch', 'scikitlearn', 'huggingface', 'lightning', 'keras',
     'pandas', 'git', 'flask', 'openai', 'fastapi', 'github', 'docker',
     'cplusplus', 'microsoftazure', 'react', 'langchain', 'gradio', 'streamlit',
