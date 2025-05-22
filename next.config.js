1ffd3a8c3bc185355cc1f1b8987c4739d7d8e329
Dennis Jonathan <dennisjonathan78@gmail.com>
2025-05-22 18:24:30 +0700

    attempt two

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'out',
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-    formats: ['image/webp'],
-    minimumCacheTTL: 60,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  }
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
