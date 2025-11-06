7e610e3b3f765bf158bbb63584e195f477129262
Dennis Jonathan <dennisjonathan78@gmail.com>
2025-11-06 14:57:26 +0700

    feat(ui): add next.js, tailwind css, and azure to skill icons

--- data/skillContent.ts
+++ data/skillContent.ts
@@ -2,5 +2,6 @@ export const skillIcons = [
     'python', 'postgresql', 'mongodb', 'javascript', 'html5', 'typescript',
     'tensorflow', 'pytorch', 'scikitlearn', 'huggingface', 'lightning', 'keras',
     'pandas', 'git', 'flask', 'openai', 'fastapi', 'github', 'docker',
-    'cplusplus', 'react', 'langchain', 'streamlit', 'opencv'
+    'cplusplus', 'react', 'langchain', 'streamlit', 'opencv', 'nextdotjs',
+    'tailwindcss', 'azure'
 ];
