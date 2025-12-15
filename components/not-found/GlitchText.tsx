b84ac015e3ff9ba7825b72329569ef1019de8731
Dennis Jonathan <dennisjonathan78@gmail.com>
2025-12-15 17:07:43 +0700

    feat: Revamp glitch text effect with a new cyberpunk style, RGB channel splitting, and hover-triggered intense animations.

--- components/not-found/GlitchText.tsx
+++ components/not-found/GlitchText.tsx
@@ -1,7 +1,12 @@
 export function GlitchText({ children }: { children: string }) {
     return (
-        <span className="glitch-text relative inline-block" data-text={children}>
-            {children}
+        <span className="glitch-wrapper group relative inline-block">
+            <span
+                className="glitch-text relative inline-block"
+                data-text={children}
+            >
+                {children}
+            </span>
         </span>
     );
 }
