3ef4b35805ad002b6ac5c4e2d10ce32bc7643c3e
Dennis Jonathan <dennisjonathan78@gmail.com>
2026-01-20 00:18:52 +0700

    refactor(ui): remove obsolete components and performance constants
    Remove unused UI components including AboutContent, BentoCard, BentoGrid, ScrollButton, ProjectDescription, and Spinner, along with the performance constants file. Update the shared index to reflect the removal of Spinner export. This cleanup continues the UI modernization efforts by eliminating dead code.

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,6 +1,5 @@
 export { default as BackToTop } from './BackToTop';
 export { default as GradientUnderline } from './GradientUnderline';
-export { default as Spinner } from './Spinner';
 export { default as ScrollRestorer } from './ScrollRestorer';
 export { default as ReadingProgress } from './ReadingProgress';
 export { default as Footer } from './Footer';
